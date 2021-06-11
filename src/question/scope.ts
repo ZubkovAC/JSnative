d4714da80dcb345694231452b5e6fa450a48cda9
ZubkovAC <3y6kob90@gmail.com>
2021-06-11 23:12:56 +0300

    native-v13.3

---
+++ src/question/scope.ts
@@ -0,0 +1,101 @@
+
+// 1)
+// if (true){
+//     var a = 2
+// }
+// function vodka (){
+//     console.log(a)
+//     a = 4
+// }
+// console.log(a)
+// vodka()
+// console.log(a)
+
+
+// 2)
+// let a = 1
+// console.log(a)
+// console.log(b)
+// var b = 2
+// console.log(b)
+// console.log(c)
+// let c = 10
+// var d
+// console.log(d)
+// d = 10
+// console.log(d)
+// console.log(e)
+// console.log(c)
+// var e = 100
+
+
+// 3)
+// console.log(a)
+// var a = 10
+// console.log(a)
+// function a (){
+//     console.log('hello')
+// }
+// console.log(a)
+
+
+// 4-5)?
+// var a = 50
+// function f(){
+//     a = 100
+//     let b= 150
+// }
+// if(true){
+//     a= 175
+//     let c = 200
+//     var d = 300
+//     const e = 400
+// }
+// f()
+//да нет
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+
+
+// 6)
+// const man1 = {
+//     fname1:'petr'
+// }
+// const man2 = {
+//     fname2:'Cemen'
+// }
+// let fname1 = 'Egorich'
+// let fname2 = 'Petrovich'
+//
+// console.log(man1[fname1]='Petrysha')
+// console.log(man2[fname2]='Xpenova')
+
+
+// 7)
+// const man = {
+//     fname: '09'
+// }
+//
+// const obj1 = {}
+// const obj2 = {}
+// man[obj1] = 123
+// man[obj2] = 456
+//
+// console.log(man[obj1])
+
+
+// 8)
+// for (let x = 0 ; x < 10; x++){
+//     setTimeout(()=>console.log(x),1000)
+// }
+// for (var x = 0 ; x < 10; x++){
+//     setTimeout(()=>console.log(x),1000)
+// }
+
+
+
+
